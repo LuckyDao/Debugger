ecb4116f35cfc5d1f246d800fd29888c040c3d50
LuckyDao <35459931+LuckyDao@users.noreply.github.com>
2018-08-04 13:17:23 -0500

    Add files via upload
    #TODOCOMPLETE:  180729_37 - errors when running initial value, AND when running 3=>n subsequent values TRIED OLDER GECKODRIVER
    #TODOCOMPLETE:  TOOK ACTIONS:  SET ALL NEEDED VARS INSIDE FX'S TO GLOBAL -- can't change vars with same name
    #TODOCOMPLETE:  180617_14 POST requests time out - is this due to query string?  not reproducing thus far:  6/18/18 THIS IS SOLVED BY EAGER LOAD STRATEGY
    #TODOCOMPLETE:  180705_34 LANDSEND USES DTM - must figure out how to defeat it -- put in a check for _satelite --NAMESPACE IS USERS RESPONSIBILITY
    #TODOCOMPLETE:  180705_35 OLD NAVY:  https://oldnavy.gap.com will this work? --NAMESPACE IS USERS RESPONSIBILITY
    #TODOCOMPLETE:  180624_23 add a way to check values through a data layer (like tealium on Belk) --Namespace is users job

--- py/guts/ScrapingScript.py
+++ py/guts/ScrapingScript.py
@@ -1,28 +1,24 @@
 
-#TODO:  180617_10 make browser options choosable
-#TODO:  180618_15 figure out how to set headless for Chrome
-#TODO:  180618_17 Should handle SSL certificates gracefully at some point - will need to find my certificates on the computer - requests package looks like it can handle this well
+#TODO:  180617_10 LG make browser options choosable incl chrome ff and ie
+#TODO:  180618_17 LG Should handle SSL certificates gracefully at some point - will need to find my certificates on the computer - requests package looks like it can handle this well
     #https://stackoverflow.com/questions/18061640/ignore-certificate-validation-with-urllib3  Could switch between HTTPSConnectionPool and PoolManager
     #HACK:  https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
-#TODO:  180623_19 get chrome to work
-#TODO:  180623_20 Rather than try except, for non critical errors, I could log them
-#TODO:  180623_21 Replace sleep with wait http://selenium-python.readthedocs.io/waits.html
-#TODO:  180624_23 add a way to check values through a data layer (like tealium on Belk)
+#TODO:  180623_20 MD Rather than try except, for non critical errors, I could log them
     #updating s_code check - if it finds utag.js, have to go into the utag code itself?  Not sure
-#TODO:  180624_24 Should I use the htmlUnit driver rather than FF or Chrome default? https://www.seleniumhq.org/docs/03_webdriver.jsp#selenium-webdriver-s-
-#TODO:  180630_28 Sometimes I receive a 504 or 304 error, and my URL if returns a "STATUS OK"  this is incorrect
-#TODO:  180630_29 print my evars/props, but also pass them into arrays
-#TODO:  180630_30 Randomize the sleep amount, but use implicit wait (todo 21)
+#TODO:  180624_24 VLG Should I use the htmlUnit driver rather than FF or Chrome default? https://www.seleniumhq.org/docs/03_webdriver.jsp#selenium-webdriver-s-
+#TODO:  180630_28 MD Sometimes I receive a 504 or 304 error, and my URL if returns a "STATUS OK"  this is incorrect --503 IS FOR CHECKOUT PAGES - SHOULD KEEP THESE
+#TODO:  180630_29 SM print my evars/props, but also pass them into arrays
+#TODO:  180630_30 SM Randomize the sleep amount, but use implicit wait (todo 21)
+    #wait http://selenium-python.readthedocs.io/waits.html
+#TODO: 180705_32 LG check if an instance of the browser is already open, if so, don't open a new one
+#TODO:  180705_33 MD LANDSEND DOESN"T WORK - it has a popup modal - must kill the popup modal --STILL AN ISSUE??? CHECK
+#TODO:  180730_37 SM change globals.* to glb.*
+#TODO:  180730_38 MD MOBILE UA ISN'T WORKING?
+#TODO:  180730_39 LG TKINTER crashes hard with a quit() fx.  Should remove these, and have better error handling
 
-#TODO: 180705_32 check if an instance of the browser is already open, if so, don't open a new one
-#TODO:  180705_33 LANDSEND DOESN"T WORK - it has a popup modal - must kill the popup modal
-#TODO:  180705_34 LANDSEND USES DTM - must figure out how to defeat it -- put in a check for _satelite
-#TODO:  180705_35 OLD NAVY:  https://oldnavy.gap.com will this work?
-
-#TODO:  180713_36:  I need to learn more about the geckodriver and how selenium/webdriver talks to it - what is doing the translation of "general.useragent.override"
 
 
-print("**ACTUAL CODE...Last Commit:  7/29/2018 14:12PM")
+print("**ACTUAL CODE...Last Commit:  8/4/18 1:18PM")
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -31,6 +27,9 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities #
 from time import sleep #allows for waiting w/ promise
 import requests #used for URL functions and validity
 import urllib3 #used to kill Insecure https request warnings- should solve properly for this
+import globals
+
+
 
 print("...Import complete...")
 
@@ -45,14 +44,8 @@ caps.update({'pageLoadStrategy':'eager'})
 ops.headless=True
 prof.set_preference("general.useragent.override", "Android 4.4; Mobile;")
 
-
-#SET TARGET URLs AND BROWSER
-URLArray = [""]
-URLCount = 0
-TargetURL = URLArray[URLCount]
-####
-
-browser = webdriver.Firefox(executable_path="C:\\Python36\\Env\\PacketSniffer\\geckodriver.exe",options=ops, capabilities=caps)
+#required geckodriver v20???  May end timeout issues
+browser = webdriver.Firefox(executable_path="C:\\Python36\\Env\\PacketSniffer\\Drivers for Selenium Library\\geckodriver20.exe",options=ops, capabilities=caps, timeout=60)
 print("...Globals are set...")
 
 
@@ -63,7 +56,7 @@ def URLTest():
     try:
         #hard disable SSL warning
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        r = requests.head(TargetURL, verify=False)
+        r = requests.head(globals.TargetURL, verify=False)
         #URL Status ok?
         if r.status_code == 200 or 301 or 302:
             print("\nURL STATUS OK:  "+str(r.status_code))
@@ -77,8 +70,8 @@ def URLTest():
 def StandardCodeChecks():
     # TEST FOR ADOBE COOKIE AND S_CODE
     #check for cookie, if not, quit.  Should build if cookie changes page to page as well
-    if browser.get_cookie("") is not None:
-        InitialVisitorCookie = browser.get_cookie("")
+    if browser.get_cookie("AMCV_F0EF5E09512D2CD20A490D4D%40AdobeOrg") is not None:
+        InitialVisitorCookie = browser.get_cookie("AMCV_F0EF5E09512D2CD20A490D4D%40AdobeOrg")
         #FUTURE STATE:  if CurrentCookie == NULL Then Continue else if Initial Visitor Cookie != CurrentCookie throw error
         print("...Cookie Was Found...")
         sleep(2)
@@ -99,9 +92,8 @@ def StandardCodeChecks():
         print("s_code does not exist!...")
         return quit()
 
-#put in a printit argument to print results or not
+
 def ScriptExecution(SVarStr,PrintMe=True):
-    #put a check for _sattelite
     SVarVal = browser.execute_script("return ("+"s."+SVarStr+");")
     if SVarVal is None:
         nothing=0
@@ -112,17 +104,17 @@ def ScriptExecution(SVarStr,PrintMe=True):
     return SVarVal
 
 def PullData():
-    browser.get(TargetURL)
+    browser.get(globals.TargetURL)
     print("...Getting URL...")
     sleep(1)
 
     #RUN STANDARD CHECKS FOR ADOBE CODE
-    StandardCodeChecks()
+    #StandardCodeChecks()
 
     try:
-        #print("********ScriptExecution*******")
         print("**************")
         ScriptExecution("pageName")
+        ScriptExecution("pageURL")
         print("**************")
         ScriptExecution("marketingCloudVisitorID")
         ScriptExecution("products")
@@ -131,18 +123,23 @@ def PullData():
             ScriptExecution("eVar" + str(i))
 
     except WebDriverException as e:
-        print("WebDriver Exception on {}".format(TargetURL))
+        print("WebDriver Exception on {}".format(globals.TargetURL))
         print("\n"+str(e))
 
 
+def KesselRun():
+    print("\nsrc Array:  " + str(globals.URLArray))
+    print("src Targ:  "+str(globals.TargetURL))
+    print("src Cnt:  " + str(globals.URLCount))
+    for m in range(0, len(globals.URLArray)):
+        globals.TargetURL = globals.URLArray[m]
+        URLTest()
+        PullData()
+        m=m + 1
+    else:
+        globals.URLArray = [""]
+        globals.URLCount = 0
+        print("\n12 Parsecs!")
 
-for m in range(0, len(URLArray)):
-    TargetURL = URLArray[URLCount]
-    URLTest()
-    PullData()
-    m += 1
-    URLCount = URLCount + 1
-else:
-    browser.quit()
-
-print("\nEND OF CODE")
+#KesselRun(URLArray, URLCount, TargetURL)
+print("End of ScrapeScript")
