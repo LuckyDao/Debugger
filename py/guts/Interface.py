589e4c93c054bb393df3b9d84a70c68bafee4751
LuckyDao <35459931+LuckyDao@users.noreply.github.com>
2018-08-04 13:14:29 -0500

    Add files via upload
    #TODOCOMPLETE:  180715_2 allow user to enter their org ID
    #TODOCOMPLETE:  180715_1 add interface that accepts an array of URLs
    #TODOCOMPLETE:  180722_3 Make interface prettier
    #TODOCOMPLETE:  180729_6 clean code up and match my other commenting style
        #Also, encapsulate some of the widgets and such so it's easier to go through
    #TODOCOMPLETE:  180724_5 clean input prior to passing
    #TODOCOMPLETE:  180724_7 have button to choose output location
    #TODOCOMPLETE:  180729_13 Get path to geckodriver
    #TODOCOMPLETE:  180722_4 Need to be able to interact with other file variables
    #TODOCOMPLETE:  180724_5 clean input prior to passing data - after cleaning input, re-set the URLs in the box for the user
    #TODOCOMPLETE:  180729_12 on focus of Adobe Org ID, clear text
    #TODOCOMPLETE:  180729_15 for some reason the interface blocks the rest of python code from firing - how to make that work?

--- py/guts/Interface.py
+++ py/guts/Interface.py
@@ -1,18 +1,16 @@
-#TODO:  180715_2 allow user to enter their org ID
-#TODO:  180715_1 add interface that accepts an array of URLs
-#TODO:  180722_3 Make interface prettier
-#TODO:  180722_4 Need to be able to interact with other file variables
-#TODO:  180724_5 clean input prior to passing
-    #resources:
-        #https://docs.python.org/3/library/tk.html
-        #Best explanation so far:  https://www.python-course.eu/tkinter_labels.php
-        #think structure is Tk is master of all, Frame is the window itself, you have to call "pack" to get widgets into the frame
-        #Example TKinter I used https://www.youtube.com/watch?v=RJB1Ek2Ko_Y&t=0s&index=2&list=PL6gx4Cwl9DGBwibXFtPtflztSNPGuIB_d
-        #http://effbot.org/tkinterbook/frame.htm  Great basics site
-        #https://coolors.co/export/pdf/224587-bcbdc0-565857-8a8d91-5e77a7
-        #https://docs.python.org/3/library/tkinter.ttk.html#widget
-        # Good for sizing of windows:  https://smallguysit.com/index.php/2017/11/11/tkinter-entry-widget/
-        #Full tkinter guide https://infohost.nmt.edu/tcc/help/pubs/tkinter/web/index.html
+
+
+#TODO:  180729_8 VLG right frame should contain output of current execution or some sort of indicator
+#TODO:  180729_9 LG in the Adobe org ID, check if it is URL encoded or not and adjust  (script prefers URL encoded)
+#TODO:  180729_8 SM sort all widget grid and creation top to bottom, left to right
+#TODO:  180729_10 MD no need to disable widgets while running, should give user feedback that it is not just hung
+#TODO:  180729_11 SM attach namespace entry box
+#TODO:  180729_14 SM outpath should be toggled as 1:  file output or 2:  driver location  (yup w/ lambda)
+#TODO:  180730_17 MD put in Entry boxes that display default selections (driver and output path should have default values)
+#TODO:  180730_18 SM from todo17 in scraping script:  put path selection for SSL certificate location
+#TODO:  180730_19 SM attach Adobe Org ID
+#TODO:  180730_20 OP since I'm importing Scrape into Interface, I could make a "browser" class with methods and such
+
     #Colors:
         # #224587 Bright Blu
         # #bcbdc0 Light Grey
@@ -25,64 +23,118 @@ print("ACTUAL CODE:  Last upload 7/29/18 2:26PM")
 
 from tkinter import *
 from tkinter import scrolledtext
+from tkinter import filedialog
+import re
+import globals
+import ScrapingScript as SS
+import os
+
+
 print ("***import complete***")
 
+#TOP LVL FXS
+def DeleteEntry(event):
+    if OrgIDEnt.get() == "Enter your Adobe Org ID":
+        OrgIDEnt.delete(0,END)
+    else:
+        x=0
+
+#test variations of this - cannot change global vars in separate file and access them here
+#GLOBAL means "global to the file" not the package
+def GetInputBox():
+    patterns = [" ", "\n", "\t"]
+    BoxData = InputBox.get("1.0",END)
+    for i in range(len(patterns)):
+        BoxData = re.sub(patterns[i],"",BoxData)
+    BoxData = re.split(",",BoxData)
+    globals.URLArray = BoxData
+    globals.TargetURL= globals.URLArray[globals.URLCount]
+    print("\nint BOX:  "+str(BoxData))
+    print("int Array:  "+str(globals.URLArray))
+    print("int Targ:  "+str(globals.TargetURL))
+    print("int Cnt:  "+str(globals.URLCount))
+    SS.KesselRun()
+    #BoxData = ""
+
+def QuitIt():
+    SS.browser.stop_client()
+    SS.browser.quit()
+    root.destroy()
+
+def SetOutputPath(param):
+    if param is "output":
+        #OutputPath = filedialog.askdirectory()
+        print("output")
+    else:
+        print("driver")
+
+
+#SET ROOT WINDOW AND CONFIGURE
 root = Tk()
-root.focus_set()
+
 root.configure(bg="#565857")
-root.geometry("800x600")
+root.geometry("1250x765")
 root.resizable(width=False, height=False)
 
 
-#TODO:  180724_5 clean input prior to passing
-#need to clean values
-# def myFunc():
-#     data = box.get("1.0",END)
-#     print("DATA:  "+data)
-#     data.strip(" ")
-#     print("STrippted:  "+data)
-#     MyArray = data.split(",")
-#     print(MyArray)
-#     return MyArray
-
-#Instantiate Frames
+#CREATE 1ST LEVEL WIDGETS (FRAMES)
 TopFrame=Frame(root)
 LeftFrame=Frame(root, bg= "#5e77a7")
 RightFrame=Frame(root,bg="#8a8d91")
 BottomFrame=Frame(root)
 
-#Frames options
-
-
-#Grid frames
+#PLACE 1ST LEVEL WIDGETS
 TopFrame.grid(row=0, column=0,padx=5, pady=5, sticky = "NWE")
-BottomFrame.grid(row=2, padx=5, pady=5,sticky="EW")
+BottomFrame.grid(row=2, column = 0, padx=5, pady=5,sticky="EW")
 LeftFrame.grid(row=1, column=0,padx=5, pady=5, sticky = "W")
 RightFrame.grid(row=1, column=1, padx=5, pady=5, sticky = "NE")
 
-#Large input text box
+#CREATE 2ND LEVEL WIDGETS (BUTTONS AND INPUT FIELDS)
 InputBox = scrolledtext.ScrolledText(LeftFrame, wrap = WORD)
+RunButton = Button(BottomFrame, text="Run Script", width=12, command = GetInputBox, bg="#5e77a7")
+QuitButton = Button(BottomFrame, text ="Quit", width =12, command = QuitIt)
+OutputButton = Button(BottomFrame, text = "Save To...", width = 12, command = lambda: SetOutputPath("output"))
+DriverButton = Button(BottomFrame, text = "Driver Path...", width = 12, command = lambda: SetOutputPath("driver"))
 
-#Buttons for passing values, quit, Device, and choosing output location
-RadioVar = IntVar()
-RunButton=Button(BottomFrame, text="Run Script", width=12)
-DeviceButtonDT = Radiobutton(BottomFrame, text = "Desktop", variable = RadioVar, value=1)
-DeviceButtonTB = Radiobutton(BottomFrame, text = "Tablet", variable = RadioVar, value=2)
-DeviceButtonSP = Radiobutton(BottomFrame, text = "Smartphone", variable = RadioVar, value=3)
+RadioVar = IntVar() #required for radio button
+DeviceRadioDT = Radiobutton(BottomFrame, text = "Desktop", variable = RadioVar, value=1)
+DeviceRadioTB = Radiobutton(BottomFrame, text = "Tablet", variable = RadioVar, value=2)
+DeviceRadioSP = Radiobutton(BottomFrame, text = "Smartphone", variable = RadioVar, value=3)
+RadioVar.set(1) #default DT
 
-#button placement and inputs
+OrgIDDefText = StringVar()
+OrgIDDefText.set("Enter your Adobe Org ID")
+OrgIDEnt = Entry(TopFrame, textvariable=OrgIDDefText, fg="#8a8d91", width=30)
+OrgIDEnt.bind("<Button-1>", DeleteEntry)
+
+NamespaceEnt = Entry(TopFrame, fg = "#8a8d91", width=30)
+
+#PLACE 2ND LEVEL WIDGETS
 InputBox.grid(row=0, column=0, padx=5, pady=5)
-RunButton.grid(row=1, column=0, padx=10, pady=5)
-DeviceButtonDT.grid(row=1, column=1, padx=5, pady=5, sticky = "W")
-DeviceButtonSP.grid(row=2, column=1, padx=5, pady=5, sticky = "W")
-DeviceButtonTB.grid(row=3, column=1, padx=5, pady=5, sticky = "W")
 
-#Create Labels
+RunButton.grid(row=1, column=0, padx=5, pady=5)
+QuitButton.grid(row=2, column=0, padx=5, pady=5)
+OutputButton.grid(row=3, column=0, padx=5, pady=5)
+DriverButton.grid(row=4, column=0, padx=5, pady=5)
+
+DeviceRadioDT.grid(row=1, column=1, padx=5, pady=5, sticky = "W")
+DeviceRadioSP.grid(row=2, column=1, padx=5, pady=5, sticky = "W")
+DeviceRadioTB.grid(row=3, column=1, padx=5, pady=5, sticky = "W")
+
+OrgIDEnt.grid(row=0,column =1, padx=5, pady=5, sticky = "W")
+NamespaceEnt.grid(row = 1, column=1, padx=5, pady=5, sticky = "W")
+
+#CREATE 2ND LEVEL LABELS
 DeviceLab = Label(BottomFrame, text = "Select Device",fg = "#224587")
 RunLab = Label(BottomFrame, text = "Begin Script",fg = "#224587")
+OrgIDLab = Label(TopFrame,text = "Org ID:  ", fg ="#224587")
+NamespaceLab = Label(TopFrame, text= "Var Namespace:  ", fg="#224587")
+
+#PLACE 2ND LEVEL LABELS
+RunLab.grid(row=0,column =0, padx=5, pady=5, sticky = "")
 DeviceLab.grid(row=0,column =1, padx=5, pady=5, sticky = "W")
-RunLab.grid(row=0,column =0, padx=5, pady=5)
+OrgIDLab.grid(row=0,column =0, padx=5, pady=5, sticky = "W")
+NamespaceLab.grid(row=1,column =0, padx=5, pady=5, sticky = "W")
 
-
-#final Run
+#FINAL RUN
 root.mainloop()
